f070cb152d9925d4bfc62677c536708e9c69b446
lucafaraldi <lucafaraldi@gmail.com>
2025-02-17 02:58:47 +0100

    WIP: local changes

--- cli.py
+++ cli.py
@@ -4,10 +4,10 @@ import spacy.cli
 
 def download_resources():
     """Download all required NLTK and spaCy resources."""
-    print("Downloading NLTK resources...")
+    print("Downloading NLTK resources...(vader_lexicon, punkt_tab, averaged_perceptron_tagger_eng)")
     nltk.download('vader_lexicon')
     nltk.download('punkt_tab')
-    nltk.download('averaged_perceptron_tagger')
+    nltk.download('averaged_perceptron_tagger_eng')
     print("Downloading spaCy model 'en_core_web_sm'...")
     spacy.cli.download("en_core_web_sm")
     print("All resources downloaded successfully.")
